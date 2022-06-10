08db2802ea17f57d5fcef276c19503e9471e0437
saadwaqas688 <saadwaqas688@gmail.com>
2022-06-10 13:30:53 +0500

    minnor changes

--- src/Test/Test.jsx
+++ src/Test/Test.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import BankAcountIcon from '../assets/icons/BankAcountIcon.jsx'
-import PasswordIcon from '../assets/icons/PasswordIcon.jsx'
-import SalesBoard from '../Components/Containers/Profile/SalesBoard/SalesBoard.jsx'
 const Test = () => {
   return (
     <div>   
