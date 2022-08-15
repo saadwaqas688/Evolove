ce2f9a8ba302c13f7961de53462bf8417fc885eb
saadwaqas688 <saadwaqas688@gmail.com>
2022-08-15 17:06:12 +0500

    remove space form folder name

--- src/Components/Containers/NewSubmission/NewSubmissionMain/NewSubmission.jsx
+++ src/Components/Containers/NewSubmission/NewSubmissionMain/NewSubmission.jsx
@@ -4,7 +4,7 @@ import UploadBlogForm from "../BlogSubmission/UploadBlogForm/UploadBlogForm";
 import CourseVideoAndImageUpload from "../CourseSubmission/CourseVideoAndImageUpload/CourseVideoAndImageUpload";
 import CreateCourseMoudle from "../CourseSubmission/CreateCourseModule/CreateCourseModule";
 import NewSubmissionCategoryForm from "../NewSubmissionCategoryForm/NewSubmissionCategoryForm";
-import ProductSubmitForm from "../ProductSubmission /ProductSubmitForm";
+import ProductSubmitForm from "../ProductSubmission/ProductSubmitForm";
 import { Heading, MainContainer } from "./NewSubmission.style";
 
 const NewSubmission = () => {
